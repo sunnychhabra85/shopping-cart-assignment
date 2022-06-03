12167abb507345f2168b8eb11c7abad173370cd6
sunny.chhabra <sunny.chhabra@santosh.ac.in>
2022-06-03 07:43:49 +0530

    shopping cart icon with drop down

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,15 @@ import { Link, Outlet } from "react-router-dom";
 import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo} from "../../assets/crown.svg";
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation =()=>{
     // const {currentUser, setCurrentUser} = useContext(UserContext);
     const {currentUser} = useContext(UserContext);
+    const {isCartOpen} = useContext(CartContext)
     // const signOutHandler = async () => {
     //   await signOutUser();
     //   setCurrentUser(null);
@@ -31,8 +34,9 @@ const Navigation =()=>{
                     SIGN IN {''}
                 </Link>)
                 }
-               
+               <CartIcon />
             </div>
+            {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </>
