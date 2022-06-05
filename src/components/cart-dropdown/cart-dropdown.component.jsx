b9dd59c9af44f1d9dc778be1b3e20f7ea8379c2f
sunny.chhabra <sunny.chhabra@santosh.ac.in>
2022-06-05 07:45:16 +0530

    shopping cart icon with drop down

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
@@ -7,6 +8,11 @@ import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () =>{
     const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () =>{
+        navigate('/checkout')
+    }
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -14,7 +20,7 @@ const CartDropdown = () =>{
                     cartItems.map((item) =><CartItem key={item.id} cartItem={item} />)
                 }
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     )
 }
